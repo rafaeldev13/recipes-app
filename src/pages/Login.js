ccca5973afb1ab379d17c91ed57acd5152e2652f
Juliana Ribeiro <silva.jurib@gmail.com>
2022-08-19 16:12:14 -0300

    requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,22 @@ import React from 'react';
 function Login() {
   return (
     <div>
-      Login
+      <input
+        data-testid="email-input"
+        type="email"
+        placeholder="Digite seu email"
+      />
+      <input
+        data-testid="password-input"
+        type="password"
+        placeholder="Digite sua senha"
+      />
+      <button
+        data-testid="login-submit-btn"
+        type="button"
+      >
+        Entrar
+      </button>
     </div>
   );
 }
