87f30c0262af41f2732221edb7267222b0a3c61c
LucasLopes <lopes.sp.lucas@gmail.com>
2022-08-20 14:15:02 -0300

    resolve requisitos 16, 17 e 18

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
+import Recipes from './pages/Recipes';
+import Profile from './pages/Profile';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
+import RecipesProvider from './context/RecipesProvider';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={ Login } />
-    </Switch>
+    <RecipesProvider>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/foods" component={ Recipes } />
+        <Route exact path="/drinks" component={ Recipes } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/done-recipes" component={ DoneRecipes } />
+        <Route path="favorite-recipes" component={ FavoriteRecipes } />
+      </Switch>
+    </RecipesProvider>
   );
 }
 
