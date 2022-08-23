12a9c597e67f0d23c1b2b4c6303e4640dd8968e5
LucasLopes <lopes.sp.lucas@gmail.com>
2022-08-22 23:00:56 -0300

    resolve requisitos 25 e 26

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { fetchRecipe } from '../services/getAPI';
+import DetailCards from '../components/DetailCards';
 
 function RecipesDetails() {
   const [currRecipe, setCurrRecipe] = useState();
@@ -9,15 +10,20 @@ function RecipesDetails() {
   const { pathname } = history.location;
   const pathArray = pathname.split('/');
   const type = pathArray[1].charAt(0).toUpperCase() + pathArray[1].slice(1);
+  const invertedType = (type === 'Foods') ? 'Drinks' : 'Foods';
   const id = pathArray[2];
 
   useEffect(() => {
     const getInfo = async () => {
       const result = await fetchRecipe(type, id);
-      console.log(result);
       setCurrRecipe(result);
     };
     getInfo();
+    // const getRecomended = async () => {
+    //   const currRecomended = await fetchOption(type, 'name', '');
+    //   setRenderRecomended(currRecomended);
+    // };
+    // getRecomended();
   }, []);
 
   const handleYoutube = (url) => {
@@ -29,6 +35,7 @@ function RecipesDetails() {
           width="80%"
           src={ newUrl }
           frameBorder="0"
+          data-testid="video"
           allow="autoplay; encrypted-media"
           allowFullScreen
           title="video"
@@ -37,11 +44,43 @@ function RecipesDetails() {
     );
   };
 
+  const mapIngMea = (array) => (
+    array.map((string, index) => (
+      <p
+        key={ index }
+        data-testid={ `${index}-ingredient-name-and-measure` }
+      >
+        {string}
+      </p>))
+  );
+
+  const handleIngMeaFood = (data) => {
+    const filteredIngredients = data.filter((key) => key[0]
+      .includes('strIngredient') && key[1] !== '');
+    const ingArray = filteredIngredients.reduce((acc, value) => [...acc, value[1]], []);
+    const filteredMeasures = data.filter((key) => key[0]
+      .includes('strMeasure') && key[1] !== ' ');
+    const meaArray = filteredMeasures.reduce((acc, value) => [...acc, value[1]], []);
+    const arrayToMap = ingArray.map((ing, index) => `- ${ing} - ${meaArray[index]}`);
+
+    return mapIngMea(arrayToMap);
+  };
+
+  const handleIngMeaDrink = (data) => {
+    const filteredIngredients = data.filter((key) => key[0]
+      .includes('strIngredient') && key[1] !== null);
+    const ingArray = filteredIngredients.reduce((acc, value) => [...acc, value[1]], []);
+    const filteredMeasures = data.filter((key) => key[0]
+      .includes('strMeasure') && key[1] !== null);
+    const meaArray = filteredMeasures.reduce((acc, value) => [...acc, value[1]], []);
+    const arrayToMap = ingArray.map((ing, index) => `- ${ing} - ${meaArray[index]}`);
+
+    return mapIngMea(arrayToMap);
+  };
+
   const recipeRender = () => {
     const LowerType = currRecipe.drinks ? 'drinks' : 'meals';
     const upperType = currRecipe.drinks ? 'Drink' : 'Meal';
-    const ingMeaArray = undefined;
-    console.log(ingMeaArray);
     return (
       <>
         <img
@@ -53,17 +92,24 @@ function RecipesDetails() {
         <h2 data-testid="recipe-title">
           { currRecipe[LowerType][0][`str${upperType}`] }
         </h2>
-        <h3 data-testid="recipe-category">
-          { currRecipe[LowerType][0].strCategory }
-        </h3>
-        <ul>
-          {}
-        </ul>
+        {currRecipe.meals
+          ? (
+            <h3 data-testid="recipe-category">
+              { currRecipe[LowerType][0].strCategory }
+            </h3>)
+          : (
+            <h3 data-testid="recipe-category">
+              { currRecipe[LowerType][0].strAlcoholic }
+            </h3>) }
+        { currRecipe.drinks
+          ? handleIngMeaDrink(Object.entries(currRecipe[LowerType][0]))
+          : handleIngMeaFood(Object.entries(currRecipe[LowerType][0])) }
         <p data-testid="instructions">
           { currRecipe[LowerType][0].strInstructions }
         </p>
         { currRecipe[LowerType][0].strYoutube
           && handleYoutube(currRecipe[LowerType][0].strYoutube) }
+        {<DetailCards typeOf={ invertedType } />}
       </>
     );
   };
